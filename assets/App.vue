168ead83aa0014a0aeede55edc731cd87e51b27a
wyksean3310 <128277471+sean-wyk@users.noreply.github.com>
2025-07-20 16:55:13 +0800

    Update App.vue

--- assets/App.vue
+++ assets/App.vue
@@ -54,12 +54,19 @@
           @contextmenu.prevent
         >
           <div class="file-icon">
-            <img
-              src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/folder/materialicons/36dp/2x/baseline_folder_black_36dp.png"
+            <!-- 替换为内联SVG文件夹图标 -->
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
               width="36"
               height="36"
               alt="Folder"
-            />
+            >
+              <path
+                fill="#000000"
+                d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"
+              />
+            </svg>
           </div>
           <span class="file-name">..</span>
         </div>
@@ -75,12 +82,19 @@
           "
         >
           <div class="file-icon">
-            <img
-              src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/folder/materialicons/36dp/2x/baseline_folder_black_36dp.png"
+            <!-- 替换为内联SVG文件夹图标 -->
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
               width="36"
               height="36"
               alt="Folder"
-            />
+            >
+              <path
+                fill="#000000"
+                d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"
+              />
+            </svg>
           </div>
           <span
             class="file-name"
